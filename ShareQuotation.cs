942ac36451ab05c5ef4648ca9b8c787379347f3f
SANGWOO PARK <cyberprophet@hanmail.net>
2024-05-20 00:27:48 +0900

    abstract the cryptocurrency exchange API

--- ShareQuotation.cs
+++ ShareQuotation.cs
@@ -1,5 +1,50 @@
-﻿namespace ShareInvest.Crypto;
+﻿using RestSharp;
 
-public abstract class ShareQuotation
+using System.Text;
+
+namespace ShareInvest.Crypto;
+
+public abstract class ShareQuotation : RestClient
 {
+    public ShareQuotation(string baseUrl) : base(baseUrl)
+    {
+
+    }
+
+    public ShareQuotation(string baseUrl, string authorizationToken) : base(baseUrl, configureDefaultHeaders: headers =>
+    {
+        headers.Add(KnownHeaders.Authorization, $"Bearer {authorizationToken}");
+    })
+    {
+
+    }
+
+    /// <summary>
+    /// UPbit
+    /// bithumb
+    /// </summary>
+    public virtual async Task<RestResponse> GetTickerAsync(params string[] codeArr)
+    {
+        StringBuilder sb = new();
+
+        foreach (string code in codeArr)
+        {
+            sb.Append(code);
+            sb.Append(',');
+        }
+        var request = new RestRequest($"ticker?markets={sb.Remove(sb.Length - 1, 1)}");
+
+        return await ExecuteAsync(request, cts.Token);
+    }
+
+    /// <summary>
+    /// UPbit
+    /// bithumb
+    /// </summary>
+    public virtual async Task<RestResponse> GetMarketAsync(bool isDetails = true)
+    {
+        return await ExecuteAsync(new RestRequest($"market/all?isDetails={isDetails}"), cts.Token);
+    }
+
+    readonly CancellationTokenSource cts = new();
 }
---
+++ ShareWebSocket.cs
@@ -0,0 +1,77 @@
+﻿using Newtonsoft.Json.Linq;
+
+using System.Net.WebSockets;
+using System.Text;
+
+namespace ShareInvest.Crypto;
+
+public abstract class ShareWebSocket<Ticker>(string baseUrl) : IDisposable where Ticker : class
+{
+    public event EventHandler<Ticker>? SendTicker;
+
+    public virtual async Task RequestAsync(string json)
+    {
+        await socket.SendAsync(new ArraySegment<byte>(Encoding.UTF8.GetBytes(json)), WebSocketMessageType.Text, true, cts.Token);
+    }
+
+    public virtual async Task ReceiveAsync()
+    {
+        while (WebSocketState.Open == socket.State)
+        {
+            var buffer = new byte[0x400];
+
+            var res = await socket.ReceiveAsync(new ArraySegment<byte>(buffer), cts.Token);
+
+            var str = Encoding.UTF8.GetString(buffer, 0, res.Count);
+
+            if (string.IsNullOrEmpty(str))
+            {
+                continue;
+            }
+            var jToken = JToken.Parse(str);
+
+            if (string.IsNullOrEmpty(jToken.Value<string>("status")))
+            {
+                switch (Activator.CreateInstance(typeof(Ticker), str))
+                {
+                    case Ticker ticker:
+                        SendTicker?.Invoke(this, ticker);
+                        continue;
+                }
+            }
+            Console.WriteLine(jToken);
+        }
+    }
+
+    /// <summary>
+    /// UPbit: api.upbit.com/websocket/v1
+    /// bithumb: pubwss.bithumb.com/pub/ws
+    /// </summary>
+    public virtual async Task ConnectAsync(string? token, TimeSpan? interval = null)
+    {
+        if (string.IsNullOrEmpty(token) is false)
+        {
+            socket.Options.SetRequestHeader("Authorization", $"Bearer {token}");
+        }
+        socket.Options.KeepAliveInterval = interval ?? TimeSpan.MinValue;
+
+        await socket.ConnectAsync(new Uri($"wss://{baseUrl}"), cts.Token);
+    }
+
+    public void Dispose()
+    {
+        socket.Dispose();
+
+        GC.SuppressFinalize(this);
+    }
+
+    protected ClientWebSocket Socket
+    {
+        get => socket;
+    }
+
+    readonly string baseUrl = baseUrl;
+
+    readonly ClientWebSocket socket = new();
+    readonly CancellationTokenSource cts = new();
+}
